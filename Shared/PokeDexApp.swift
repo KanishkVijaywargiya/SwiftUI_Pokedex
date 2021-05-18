3c61660b0d8d9cba27db4cb4114e93320136c913
Kanishk Vijaywargiya <kanishkvijaywargiya99@gmail.com>
2021-05-18 14:02:18 +0530

    Initial Commit

---
+++ Shared/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  Shared
+//
+//  Created by KANISHK VIJAYWARGIYA on 18/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
